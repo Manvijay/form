7658a660470edfb98deed5c05c8b51468470250d
Manvijay <manvijay@buffalo.edu>
2018-12-20 09:41:10 -0500

    check

--- mailHandler.php
+++ mailHandler.php
@@ -1,5 +1,12 @@
 <?php
-  mail('manvijay@buffalo.edu', 'checking php');
+$recipient = "brentw.white@gmail.com"; //my email
+$subject = 'Email message from Point Plumbing';
+$fName = $_POST ["fName"];
+$lName = $_POST ["lName"];
+
+$mailBody="Name: $name\nEmail: $email\n\n$message";
+$mail($recipient, $subject, $mailBody, "From: $fname <$email>");
+$thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
 
   if(mail('manvijay@buffalo.edu', 'checking php')){
     echo "<h1> Sent Successfully</h1>";
