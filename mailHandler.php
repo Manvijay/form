5c06c0f88d79c36a1549c07c14bd3fe61043864c
Manvijay <manvijay@buffalo.edu>
2018-12-20 09:47:05 -0500

    check

--- mailHandler.php
+++ mailHandler.php
@@ -1,14 +1,14 @@
 <?php
-$recipient = "brentw.white@gmail.com"; //my email
+$recipient = "manvijaythelegend@gmail.com"; //my email
 $subject = 'Email message from Point Plumbing';
 $fName = $_POST ["fName"];
 $lName = $_POST ["lName"];
 
 $mailBody="Name: $name\nEmail: $email\n\n$message";
-$mail($recipient, $subject, $mailBody, "From: $fname <$email>");
+mail($recipient, $subject, $mailBody, "From: $fname <$email>");
 $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
 
-  if(mail('manvijay@buffalo.edu', 'checking php')){
+  if(mail($recipient, $subject, $mailBody, "From: $fname <$email>")){
     echo "<h1> Sent Successfully</h1>";
   }
   else {
